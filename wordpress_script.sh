dc5a449c7cd95425be9fcb375b5bec2f7b2ed7b3
Khrystyna Pavlyk <khrystynapavlyk1@gmail.com>
2022-07-03 12:57:08 -0700

    Added khr_wordpress.conf

--- wordpress_script.sh
+++ wordpress_script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-echo "start apt update"
+echo "Start apt update"
 sudo apt update -y
-echo "end apt update"
+echo "End apt update"
 
-echo "start apt install "
+echo "Start apt install"
 sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
-echo "end apt install"
+echo "End apt install"
 
-echo "start systemctl restart apache2"
+echo "Start systemctl restart apache2"
 sudo systemctl restart apache2
-echo "end systemctl restart apache2 "
+echo "End systemctl restart apache2"
+
+echo "Start creating khr_wordpress.cong"
+cat << 'EOF' > /etc/apache2/sites-available/khr_wordpress.conf
+<Directory /var/www/khr_wordpress/>
+        AllowOverride All
+</Directory>
+EOF
+echo "End creating khr_wordpress.cong"
+
